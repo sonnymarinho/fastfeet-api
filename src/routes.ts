e40bca9d3071e578671a9d1d1395a84119b20ab5
Sonny Marinho <sonny.marinho@gmail.com>
2021-03-11 13:34:53 -0300

    feat: added celebrate validation

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,36 @@
+import { celebrate, Joi, Segments } from 'celebrate'
 import { createUserController } from '@useCases/createUser'
 import { authenticateUserController } from '@useCases/authenticateUser'
 import { Router } from 'express'
 
 const router = Router()
 
-router.post('/users', async (request, response) => {
-  await createUserController.handle(request, response)
-})
+router.post(
+  '/users',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+      deliveryman: Joi.boolean().required(),
+    },
+  }),
+  async (request, response) => {
+    await createUserController.handle(request, response)
+  }
+)
 
-router.post('/sessions', async (request, response) => {
-  await authenticateUserController.handle(request, response)
-})
+router.post(
+  '/sessions',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  async (request, response) => {
+    await authenticateUserController.handle(request, response)
+  }
+)
 
 export { router as routes }
